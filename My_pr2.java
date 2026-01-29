eb3b4cfbc8e5af134563e924cd35e402675005c1
piyushjha8483 <146409402+piyushjha8483@users.noreply.github.com>
2026-01-29 09:41:58 +0530

    Add files via upload
    java

---
+++ My_pr2.java
@@ -0,0 +1,36 @@
+// import java.util.*;
+public class My_pr2 {
+    public static void main (String args[]){
+        // Scanner sc = new Scanner (System.in);
+       
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b ;
+       
+        // float rad = sc.nextInt();
+        // float area = 3.14f*rad*rad;
+        // System.out.println(area);
+        // sc.close();
+
+        // float a = 45.7778f;
+        // int b = (int)a;
+
+        // char ch = 'B';
+        // int b = 'A';
+
+        // char ch = 'a';
+        // System.out.println((int)ch);
+
+        char a = 'a';
+        char b = 'b';
+        int sum = b-a;
+        System.out.println(sum);
+       
+
+
+   
+   
+   
+    }
+
+}
